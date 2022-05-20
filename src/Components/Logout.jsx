b83ced58830ee2e88618a818d58502b3b225ca56
Bradley Wilson <bwils35@gmail.com>
2022-05-20 14:24:30 -0500

    Implement styled components for the mainview

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -1,28 +1,22 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import LoginForm from "./LoginForm";
+import { LogoutButton } from "../Styles/Button.style";
+import { Page, LogoutButtonContainer } from "../Styles/Container.style";
 
 //Returns you to Main page from the stock page. Changes the values of SetShow
 const Logout = () => {
 	let navigate = useNavigate();
 	return (
 		<>
-			<div className="row">
-				<div className="col-6" />
-				<div className="col-6 mt-3">
-					<button
-						className="btn btn-danger btn-outline-dark btn-md"
-						type="submit"
-						class="LogOut"
-						onClick={() => {
-							navigate("/");
-						}}
-					>
-						Logout
-					</button>
-					<div className="col-6" />
-				</div>
-			</div>
+			<LogoutButtonContainer>
+				<LogoutButton
+					onClick={() => {
+						navigate("/");
+					}}
+				>
+					Logout
+				</LogoutButton>
+			</LogoutButtonContainer>
 		</>
 	);
 };
--- src/Styles/Button.style.jsx
+++ src/Styles/Button.style.jsx
@@ -30,13 +30,29 @@ export const SubmitButton = styled.button`
 
 export const CoinButton = styled.button`
 	border: 1px solid #000;
-	background-color: #183de4;
-	color: #fff;
+	/* background-color: #183de4; */
+	/* color: #fff; */
 	border: none;
 	border-radius: 10px;
 	box-shadow: 0px 0px 2px 2px rgb(0, 0, 0);
-	display: inline-flex;
-	position: relative;
-	right: 195px;
-	top: 0px;
+	/* display: inline-flex;
+	position: relative; */
+	/* right: 195px; */
+	/* top: 0px; */
+`;
+
+export const LogoutButton = styled.button`
+	color: white;
+	background-color: Red;
+	/* position: relative;
+	bottom: 900px;
+	left: 600px; */
+	min-width: 300px;
+	border: 1px solid #f9f2f2;
+	border-radius: 10px;
+	text-align: center;
+	line-height: 20px;
+	min-height: 50px;
+	border: 1px solid #000;
+	/* display: inline-block; */
 `;
